00753ef86104bc0ccc36c16aea30262b75150b30
KHG <hgkim@aegisdds.com>
2024-03-11 19:06:35 +0900

    scanf

---
+++ scanf/scanf/scanf.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int a;
+//int main()
+//{
+//	scanf("%d", &a);
+//	printf("입력 값 : %d\n", a);
+//
+//	return 0;
+//} //입력값 출력
+
+int main()
+{
+	int age;
+	double height;
+	char grade;
+	char name[20];
+
+	printf("이름 입력 : ");
+	scanf("%s", name)
+	return 0;
+}
+
+
