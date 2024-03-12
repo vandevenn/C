00c00cdcac9df8afa11e298d689fab43f97e9776
KHG <hgkim@aegisdds.com>
2024-03-12 10:42:48 +0900

    scanf

--- scanf/scanf/scanf.c
+++ scanf/scanf/scanf.c
@@ -9,6 +9,7 @@
 //	return 0;
 //} //입력값 출력
 
+
 int main()
 {
 	int age;
@@ -16,9 +17,21 @@ int main()
 	char grade;
 	char name[20];
 
+
+
 	printf("이름 입력 : ");
-	scanf("%s", name)
+	scanf("%s", name);
+
+	printf("학점 입력 : ");
+	scanf(" %c", &grade); //%c 앞에 공백을 넣어 개행문자(\n = 엔터)무시
+
+	printf("나이와 키 입력 : ");
+	scanf("%d%lf", &age, &height);
+
+	printf("%s의 학점은 %c입니다.\n", name, grade);
+	printf("%s의 나이는 %d살 키는 %lf입니다.", name, age, height);
 	return 0;
 }
+
 
 
