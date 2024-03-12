40348cab501ca7494fb25fc5a53e2cf3f5bcff5b
KHG <hgkim@aegisdds.com>
2024-03-12 11:30:27 +0900

    operand.2

--- operand/operand/operand.c
+++ operand/operand/operand.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
+//int main(void)
+//{
+//	int a = 5, b = 5;
+//	int pre, post;
+//
+//	pre = (++a) * 3; //증감한걸 즉시 반영
+//	post = (b++) * 3; //반영하고 증감함
+//
+//	printf("이전값 : %d,%d", a, b);
+//	printf("전위 :%d, 후위 : %d", pre, post);
+//	return 0;
+//}
+
 int main(void)
 {
-	int a = 5, b = 5;
-	int pre, post;
+	double a = 4.0, b = 1.2;
 
-	pre = (++a) * 3;
-	post = (b++) * 3;
+	printf("%.1lf + %.1lf = %.1lf \n", a, b, a + b);
+	printf("%.1lf - %.1lf = %.1lf \n", a, b, a - b);
+	printf("%.1lf * %.1lf = %.1lf \n", a, b, a * b);
+	printf("%.1lf / %.1lf = %.1lf \n", a, b, a / b);
 
-	printf("이전값 : %d,%d", a, b);
-	printf("전위 :%d, 후위 : %d", pre, post);
 	return 0;
 }
+
+
 
