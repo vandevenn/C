d73ab0833d690bb300cfd35dfa95106151d94ae4
KHG <hgkim@aegisdds.com>
2024-03-14 15:12:00 +0900

    function

---
+++ Backjoon/Backjoon/backjoon.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int A, B, res;
+//	scanf("%d %d", &A, &B);
+//	res = A + B;
+//	printf("%d", res);
+//	return 0;
+//}
+
+int main()
+{
+	int A = 7; 
+	int B = 3;
+	int res;
+
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+	return 0;
+	
+}
