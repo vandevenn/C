14e2d2fd78110a2cf1257d840ad6322f16962942
KHG <hgkim@aegisdds.com>
2024-04-01 16:00:47 +0900

    2439

--- Backjoon/Backjoon/loop.c
+++ Backjoon/Backjoon/loop.c
@@ -20,7 +20,32 @@ void Q2438()
 	}
 }
 
+void Q2439()
+{
+	int N = 1;
+	scanf("%d", &N);
+	if ((N < 1) || (N > 100))
+	{
+		printf("1~100사이 입력");
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N - i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 1; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+}
 int main()
 {
-	Q2438();
+	//Q2438();
+	Q2439();
+	return 0;
 }
